55ac8ac4a2c47fd5b890af5090b749b645dbcc3b
Prashant <prashantd049@gmail.com>
2023-02-21 21:44:33 +0530

    Commit

--- 4-day.js
+++ 4-day.js
@@ -6,3 +6,5 @@ function getDaysBetweenDates(dateText1, dateText2) {
     // write your solution here
     return Math.abs(Math.floor((Date.parse(dateText1) - Date.parse(dateText2))/DAY_IN_MILLISECONDS))
 }
+
+console.log(`Days difference: ${getDaysBetweenDates('10/15/2020', '12/1/2020')}`)
